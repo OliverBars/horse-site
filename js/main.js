9b4ff1eabf7f9e9c517f5ee5968e7c659c0d5c7d
Гром Ярослав <111660823+OliverBars@users.noreply.github.com>
2023-05-06 18:24:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,11 +21,4 @@ $(document).ready(function () {
   });
 });
 
-$(".photo__box-slider").slick({
-  dots: false,
-  arrows:false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-});
+
